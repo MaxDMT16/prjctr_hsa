29163b45ba3a1b3ddfce862f74e127135cefaea3
Maksym <maxdmt1638@gmail.com>
2023-12-25 20:56:18 +0200

    ha20: added AVL benchmarking

---
+++ 20_bst_&_counting_sort/README.md
@@ -0,0 +1,34 @@
+# AVL tree (balanced binary search tree)
+
+| operation |items count| ps/ ec|time|
+|-------------|-----------|-----------|-----------|
+| insert | 10 | 1237154 | 961.9 ns/op |
+| insert | 100 | 80379 | 13776 ns/op |
+| insert | 1000 | 6758 | 179777 ns/op |
+| insert | 10000 | 477 | 2394143 ns/op |
+| insert | 100000 | 31 | 35982921 ns/op |
+| insert | 10 | 9543699 | 110.1 ns/op |
+| insert | 100 | 1646738 | 721.7 ns/op |
+| insert | 1000 | 163234 | 6802 ns/op |
+| insert | 10000 | 17694 | 67285 ns/op |
+| insert | 100000 | 1546 | 698540 ns/op |
+| find | 10 | 298964486 | 3.808 ns/op |
+| find | 100 | 305052837 | 3.804 ns/op |
+| find | 1000 | 307928382 | 3.773 ns/op |
+| find | 10000 | 304441972 | 3.820 ns/op |
+| find | 100000 | 305117412 | 3.796 ns/op |
+| find | 10 | 42420254 | 28.89 ns/op |
+| find | 100 | 31248565 | 39.23 ns/op |
+| find | 1000 | 22474227 | 47.85 ns/op |
+| find | 10000 | 18328552 | 59.79 ns/op |
+| find | 100000 | 14822144 | 72.85 ns/op |
+| remove | 10 | 28278637 | 40.99 ns/op |
+| remove | 100 | 20618539 | 55.23 ns/op |
+| remove | 1000 | 16050721 | 69.37 ns/op |
+| remove | 10000 | 11622096 | 92.50 ns/op |
+| remove | 100000 | 9515421 | 118.1 ns/op |
+| remove | 10 | 85232859 | 12.97 ns/op |
+| remove | 100 | 86658385 | 13.10 ns/op |
+| remove | 1000 | 84102798 | 13.02 ns/op |
+| remove | 10000 | 88649030 | 13.04 ns/op |
+| remove | 100000 | 81596511 | 13.03 ns/op |
---
+++ 20_bst_&_counting_sort/main_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/emirpasic/gods/trees/avltree"
+)
+
+type keyGeneratorFunc func() int
+
+func insert(b *testing.B, tree *avltree.Tree, n int, generateKey keyGeneratorFunc) {
+	for j := 0; j < n; j++ {
+		tree.Put(generateKey(), 1)
+	}
+}
+
+var table = []int{10, 100, 1000, 10_000, 100_000} //, 1_000_000}
+
+func BenchmarkInsert_DifferentNumbers(b *testing.B) {
+	tree := avltree.NewWithIntComparator()
+
+	for _, v := range table {
+		b.Run(fmt.Sprintf("insert_%d", v), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				insert(b, tree, v, func() int { return rand.Int() })
+
+				tree.Clear()
+			}
+		})
+	}
+}
+
+func BenchmarkInsert_SameNumber(b *testing.B) {
+	tree := avltree.NewWithIntComparator()
+
+	for _, v := range table {
+		b.Run(fmt.Sprintf("insert_%d", v), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				insert(b, tree, v, func() int { return 1 })
+
+				tree.Clear()
+			}
+		})
+	}
+}
+
+func BenchmarkFind_SameNumber(b *testing.B) {
+	tree := avltree.NewWithIntComparator()
+
+	for _, v := range table {
+		b.Run(fmt.Sprintf("find_%d", v), func(b *testing.B) {
+			insert(b, tree, v, func() int { return 1 })
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				tree.Get(1)
+			}
+		})
+	}
+}
+
+func BenchmarkFind_DifferentNumber(b *testing.B) {
+	tree := avltree.NewWithIntComparator()
+
+	for _, v := range table {
+		b.Run(fmt.Sprintf("find_%d", v), func(b *testing.B) {
+			insert(b, tree, v, func() int { return rand.Int() })
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				tree.Get(i)
+			}
+		})
+	}
+}
+
+func BenchmarkRemove_DifferentNumber(b *testing.B) {
+	tree := avltree.NewWithIntComparator()
+
+	for _, v := range table {
+		b.Run(fmt.Sprintf("remove_%d", v), func(b *testing.B) {
+			insert(b, tree, v, func() int { return rand.Int() })
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				tree.Remove(i)
+			}
+		})
+	}
+}
+
+func BenchmarkRemove_SameNumber(b *testing.B) {
+	tree := avltree.NewWithIntComparator()
+
+	for _, v := range table {
+		b.Run(fmt.Sprintf("remove_%d", v), func(b *testing.B) {
+			insert(b, tree, v, func() int { return 1 })
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				tree.Remove(i)
+			}
+		})
+	}
+}
